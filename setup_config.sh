4518a4c35b370a7e80b570d323ec1edd32724f28
Dhruv Tapasvi <dhruv.tapasvi1996@gmail.com>
2019-02-14 22:57:43 +0000

    further aliases

--- setup_config.sh
+++ setup_config.sh
@@ -3,7 +3,7 @@ export PWD=$(pwd)
 ln -sf $PWD/config/vimrc ~/.vimrc
 ln -sf $PWD/config/zshrc ~/.zshrc
 ln -sf $PWD/config/aliases ~/.aliases
-echo "# Add machine-local aliases here\n" > ~/.local_aliases
+echo "# Add machine-local aliases, and references to optional alias files, here\n" > ~/.local_aliases
 
 unset PWD
 
