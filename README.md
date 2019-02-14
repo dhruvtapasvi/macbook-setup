031a7cf83803d5d2a11e23a0c053b06e7a847f91
dhruvtapasvi <dhruvtapasvi@users.noreply.github.com>
2019-02-14 22:26:09 +0000

    Terminal setup instructions (#1)
    * Add setup terminal script
    
    * Update readme with setup instruction

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 This repository contains configuration settings and setup scripts for personalising my macbook.
 
+## Instructions
+
+Start off by running `source setup_terminal.sh` from Terminal `bash`. This creates the correct terminal environment.
+
---
+++ setup_terminal.sh
@@ -0,0 +1,12 @@
+# Install xcode command line tools:
+ xcode-select --install
+
+# Install homebrew:
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install zsh and oh-my-zsh
+brew install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+read -p "Install iTerm 2"
+
