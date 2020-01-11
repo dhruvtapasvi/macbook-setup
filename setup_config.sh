2f23578cbe88c9ad2129dfad9ba68e3ddcdeb7c8
Dhruv Tapasvi <dhruv.tapasvi1996@gmail.com>
2020-01-11 23:25:57 +0000

    git config

--- setup_config.sh
+++ setup_config.sh
@@ -1,4 +1,5 @@
 ln -sf $PWD/config/vimrc ~/.vimrc
 ln -sf $PWD/config/zshrc ~/.zshrc
 ln -sf $PWD/config/aliases ~/.aliases
+ln -sf $PWD/config/gitconfig ~/.gitconfig
 echo "# Add machine-local aliases, and references to optional alias files, here\n" > ~/.local_aliases
